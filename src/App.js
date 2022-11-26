042266db001b0515dc7548763f985938cefe2c45
Mateen Mirani <mateenmirani125@gmail.com>
2022-11-26 16:26:54 +0500

    install few packages & set up the header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        {/* Home (the one with the search on) */}
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import AppsIcon from "@mui/icons-material/Apps";
+import { IconButton } from "@mui/material";
+import Tooltip from "@mui/material/Tooltip";
+import Button from "@mui/material/Button";
+
+const Home = () => {
+  return (
+    <>
+      <div className="flex flex-col h-screen ">
+        <div className="flex justify-end items-center px-8 py-5 space-x-4 min-w-fit">
+          <Link
+            to="/gmail"
+            className=" text-base no-underline hover:underline text-[#000000b3]"
+          >
+            Gmail
+          </Link>
+          <Link
+            to="/images"
+            className=" text-base no-underline hover:underline text-[#000000b3]"
+          >
+            Images
+          </Link>
+          <Tooltip title="Google Apps">
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <AppsIcon />
+            </IconButton>
+          </Tooltip>
+          <Button
+            variant="contained"
+            size="medium"
+            sx={{ textTransform: "none" }}
+          >
+            Sign in
+          </Button>
+        </div>
+        <div className="flex-1 mt-8 flex flex-col">
+          <img
+            src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+            alt="logo"
+            className="object-contain h-24"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
